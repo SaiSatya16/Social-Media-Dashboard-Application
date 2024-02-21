b18124747d969cfa8d58bcadfd213e92b5d2119a
jj1039 <jj1039@srmist.edu.in>
2024-02-21 15:23:52 +0530

    db model update 1

--- model.py
+++ model.py
@@ -20,10 +20,43 @@ class User(db.Model, UserMixin):
     fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
     roles = db.relationship('Role', secondary='roles_users',
                             backref=db.backref('users', lazy='dynamic'))
+    posts = db.relationship('Post', backref='author', secondary='user_post_association')
 
 
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False, unique=True)
     description = db.Column(db.String, nullable=False)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.String(100), nullable=False)
+    updated_at = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100))
+    analytics = db.relationship('Analytics', backref='post', secondary='post_analytics_association')
+
+class Analytics(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+    comment = db.Column(db.Text)
+    shares = db.Column(db.Integer)
+    created_at = db.Column(db.String(100), nullable=False)
+    updated_at = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class user_post_association(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),primary_key = True, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'),primary_key = True, nullable=False)
+
+class post_analytics_association(db.Model):
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'),primary_key = True, nullable=False)
+    analytics_id = db.Column(db.Integer, db.ForeignKey('analytics.id'),primary_key = True, nullable=False)
+
+
+   
 
--- upload_initial_data.py
+++ upload_initial_data.py
@@ -1,6 +1,6 @@
 from app import app
 from sec import datastore
-from model import db, User, Role, Scheme, Vote, Association
+from model import db, User, Role
 from flask_security import hash_password
 from werkzeug.security import generate_password_hash
 
@@ -8,8 +8,7 @@ from werkzeug.security import generate_password_hash
 with app.app_context():
     db.create_all()
     datastore.find_or_create_role(name="Admin", description="user is Admin")
-    datastore.find_or_create_role(name="Representative", description="user is Representative")
-    datastore.find_or_create_role(name="Customer", description="user is Customer")
+    datastore.find_or_create_role(name="Creator", description="user is Creator")
     db.session.commit()
     if not datastore.find_user(email="admin@gmail.com"):
         datastore.create_user(
@@ -17,18 +16,12 @@ with app.app_context():
             email="admin@gmail.com",
             password= generate_password_hash("admin"),
             roles=["Admin"])
-    if not datastore.find_user(email="Representative@gmail.com"):
+    if not datastore.find_user(email="Creator@gmail.com"):
         datastore.create_user(
-            username="representative",
-            email="representative@gmail.com",
-            password=generate_password_hash("representative"),
-            roles=["Representative"], active=False)
-    if not datastore.find_user(email="customer@gmail.com"):
-        datastore.create_user(
-            username="customer",
-            email="customer@gmail.com",
-            password=generate_password_hash("customer"),
-            roles=["Customer"])
+            username="Creator",
+            email="Creator@gmail.com",
+            password=generate_password_hash("Creator"),
+            roles=["Creator"])
     db.session.commit()
 
 
